ea56cf244c33bdc9e78f1ed73974bb0afac8df4e
Robinyo <rob.ferguson@robferguson.org>
2015-06-14 15:33:10 +1000

    Added DELETE /posts/{id}

--- core/server/api/controllers/posts.js
+++ core/server/api/controllers/posts.js
@@ -1,9 +1,8 @@
 'use strict';
 
 var fs = require('fs');
-var status = require('./httpResponseStatusCodes');
+var status = require('./http-status-codes');
 
-// Post
 // Rule: POST must be used to create a new resource in a collection
 // Rule: Location must be used to specify the URI of a newly created resource
 
@@ -29,6 +28,38 @@ function postPost(req, res) {
   }
 }
 
+// Rule: GET must be used to retrieve a representation of a resource
+
+function getPosts(req, res) {
+  // serveStaticFileSync(res, '/posts.json');
+  serveStaticFile(res, '/posts.json');
+}
+
+function serveStaticFile(res, path) {
+
+  res.type('application/json');
+
+  fs.readFile(__dirname + path, 'utf8', function (error, data) {
+    if (error) {
+      res.status(status.INTERNAL_SERVER_ERROR).send('{ "code": "500", "message": "Something went wrong :(" }');
+    } else {
+      res.status(status.OK).send(JSON.parse(data));
+    }
+  });
+}
+
+function serveStaticFileSync(res, path) {
+
+  // var obj = fs.readFileSync(__dirname + path, 'utf8');
+  var obj = JSON.parse(fs.readFileSync(__dirname + path, 'utf8'));
+
+  res.type('application/json');
+  res.status(status.OK).send(obj);
+}
+
+// Rule: PUT must be used to both insert and update a stored resource
+// Rule: PUT must be used to update mutable resources
+
 function putPost(req, res) {
 
   var responseCode = status.OK;
@@ -51,36 +82,32 @@ function putPost(req, res) {
   }
 }
 
-function getPosts(req, res) {
+// Rule: DELETE must be used to remove a resource from its parent
 
-  // serveStaticFileSync(res, '/posts.json');
-  serveStaticFile(res, '/posts.json');
-}
+function deletePost(req, res) {
 
-function serveStaticFileSync(res, path) {
+  var responseCode = status.OK;
 
-  var obj = JSON.parse(fs.readFileSync(__dirname + path, 'utf8'));
-  // var obj = fs.readFileSync(__dirname + path, 'utf8');
-
-  res.type('application/json');
-  res.status(status.OK).send(obj);
-}
-
-function serveStaticFile(res, path) {
+  // do something ...
 
   res.type('application/json');
 
-  fs.readFile(__dirname + path, 'utf8', function (error, data) {
-    if (error) {
-      res.status(status.INTERNAL_SERVER_ERROR).send('{ "code": "500", "message": "Something went wrong :(" }');
-    } else {
-      res.status(status.OK).send(JSON.parse(data));
-    }
-  });
+  switch (responseCode) {
+
+    case status.OK:
+      res.status(status.OK).send('{ "id": "123456" }');
+      break;
+
+    case status.NOT_FOUND:
+    default:
+      res.status(status.BAD_REQUEST).end('{ "code": "404", "message": "Something went wrong :(" }');
+      break;
+  }
 }
 
 module.exports = {
-  post: postPost,
-  put: putPost,
-  get: getPosts
+  post: postPost,       // create
+  get: getPosts,        // retrieve
+  put: putPost,         // update
+  delete: deletePost    // delete
 }
