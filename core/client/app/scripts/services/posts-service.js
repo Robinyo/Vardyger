c98cff318557c7aa9e53c22dfbcd5678face9173
Robinyo <rob.ferguson@robferguson.org>
2015-07-17 15:52:26 +1000

    Updated variables and styles

--- core/client/app/scripts/services/posts-service.js
+++ core/client/app/scripts/services/posts-service.js
@@ -34,18 +34,29 @@ angular.module('vardyger')
               'Yeoman <a href="http://yeoman.io/generators/" target="_blank">generators</a> and projects like ' +
               '<a href="http://joshdmiller.github.io/ng-boilerplate/#/home" target="_blank">ng-boilerplate</a></p>' +
               '<h3 id="yeoman">Yeoman</h3>' +
-              '<p>Yeoman provides a <a href="http://yeoman.io/generators/" target="_blank">generator</a> ecosystem' +
+              '<p>Yeoman provides a <a href="http://yeoman.io/generators/" target="_blank">generator</a> ecosystem ' +
               'that helps you to kickstart new projects. A generator is basically a plugin that can be run with the ' +
-              '<code>yo</code> command to create the scaffolding for a new project:</p>'
+              '<code>yo</code> command to create the scaffolding for a new project:</p>',
+            markdown: 'AngularJS is the toolset that underpins the Ionic Framework.' +
+              'This means that if you want to use the Ionic Framework to develop hybrid mobile applications then you ' +
+              'will need to understand how Angular web applications are built and structured. A great place to start ' +
+              'is to take a look at the Angular project structure templates created by popular Yeoman ' +
+              '<a href="http://yeoman.io/generators/" target="_blank">generators</a> and projects like ' +
+              '<a href="http://joshdmiller.github.io/ng-boilerplate/#/home" target="_blank">ng-boilerplate</a> ' +
+              '### Yeoman' +
+              'Yeoman provides a <a href="http://yeoman.io/generators/" target="_blank">generator</a> ecosystem ' +
+              'that helps you to kickstart new projects. A generator is basically a plugin that can be run with ' +
+              'the `yo` command to create the scaffolding for a new project:'
           }
         },
         {
           metaTitle: 'Customising Ionic with Sass',
-          metaDescription: 'In a previous post, I wrote about using generator-ionic to create the scaffolding for a mobile-optimised Administration UI.',
+          metaDescription: 'In a previous post, ...',
           post: {
             id: '2',
             title: 'Customising Ionic with Sass',
-            html: '<p>In a previous post, I wrote about using generator-ionic to create the scaffolding for a mobile-optimised Administration UI.</p>'
+            html: '<p>In a previous post, ...</p>',
+            markdown: 'In a previous post, ...'
           }
         }
 
